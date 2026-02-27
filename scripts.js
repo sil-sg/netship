1bdefad7c902f92d7ce2c200562ac1c4371f246a
sil-sg <sillvano.play.sg@gmail.com>
2026-02-27 13:43:10 +0100

    feat: upgrade 2

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 
 const btnMenu = document.querySelector(".btn-menu")
+const menu = document.querySelector(".menu")
+const bgBlur = document.querySelector(".blur")
+const navLinks = document.querySelectorAll(".nav-link")
+
+navLinks.forEach(i => {
+    i.onclick = () => {
+        menu.classList.remove("active")
+        bgBlur.classList.remove("active")
+        btnMenu.classList.remove("active")
+    }
+})
 
 btnMenu.onclick = () => toggleMenu()
 
 function toggleMenu() {
-    const menu = document.querySelector(".menu")
-    const bgBlur = document.querySelector(".blur")
     
     menu.classList.toggle("active")
     bgBlur.classList.toggle("active")
